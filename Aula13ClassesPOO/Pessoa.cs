dae384d9e02beea773b0b59963a221bfc0f00172
MaiconConstancio <135743035+MaiconConstancio@users.noreply.github.com>
2023-07-26 15:18:12 -0300

    Add files via upload

---
+++ Aula13ClassesPOO/Pessoa.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula13ClassesPOO
+{
+    internal class Pessoa
+    {
+        //Atributos (variaveis)
+        public string nome;
+        public int idade;
+        public string rua;
+        public string cidade;
+
+        //Metodo construtor
+        public Pessoa(string nome, int idade, string rua, string cidade)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.rua = rua;
+            this.cidade = cidade;
+            Console.WriteLine("\n\nSalve guys fui criado");
+        }
+
+        public void mostra()
+        {
+            Console.WriteLine($"Meu nome é {nome}, tenho {idade} anos e moro na rua, {rua} na cidade de, {cidade}");
+        }
+
+
+
+
+
+    }
+
+}
---
+++ Aula13ClassesPOO/Program.cs
@@ -0,0 +1,19 @@
+﻿using Aula13ClassesPOO;
+using System.Security.Cryptography;
+
+//Console.WriteLine("Hello, World!");
+
+Pessoa Camila = new Pessoa("Camila", 19, "rua X", "são carlos");
+Pessoa Kenedy = new Pessoa("Kenedy", 17, "rua X2", "são carlos");
+
+List<Pessoa> pessoas = new List<Pessoa>();
+pessoas.Add(Camila);
+pessoas.Add(Kenedy);
+
+for (int i = 0; i < pessoas.Count; i++)
+{
+    pessoas[i].mostra();
+    //Console.WriteLine(pessoas[i].nome);
+}
+
+Console.ReadKey();
